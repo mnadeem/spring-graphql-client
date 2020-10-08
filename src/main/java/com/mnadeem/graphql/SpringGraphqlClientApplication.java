9d95c4a2baef03ee3e04727116ec02741e9860e9
Nadeem Mohammad <coolmind182006@gmail.com>
2020-10-08 11:21:52 +0530

    first commit

---
+++ src/main/java/com/mnadeem/graphql/SpringGraphqlClientApplication.java
@@ -0,0 +1,13 @@
+package com.mnadeem.graphql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGraphqlClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGraphqlClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mnadeem/graphql/SpringGraphqlClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mnadeem.graphql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGraphqlClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
